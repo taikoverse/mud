06e60f2176b16869bbb0fcd125158b739829ec37
vhjiang <scidotmoney@gmail.com>
2023-07-09 08:10:42 +0000

    fix(faucet): add twitter mode config

--- packages/services/script/start-faucet.sh
+++ packages/services/script/start-faucet.sh
@@ -7,11 +7,15 @@ if [ "$FAUCET_DEV_MODE" == "true" ]; then
         -ws-url ${WS_URL} \
         -faucet-private-key ${FAUCET_PK} \
         -dev \
-        -metrics-port ${FAUCET_METRICS_PORT}
+        -metrics-port ${FAUCET_METRICS_PORT} \
+        -twitter-bearer-token ${BEARER_TOKEN} \
+        -twitter ${IS_TWITTER_MODE}
 else
     /service/faucet \
         -ws-url ${WS_URL} \
         -faucet-private-key ${FAUCET_PK} \
-        -metrics-port ${FAUCET_METRICS_PORT}
+        -metrics-port ${FAUCET_METRICS_PORT} \
+        -twitter-bearer-token ${BEARER_TOKEN} \
+        -twitter ${IS_TWITTER_MODE}
 
 fi
